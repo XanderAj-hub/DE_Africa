68a107ca3fa8642603c85f2a8d4438c4866eaa5b
XANDER <alexasamoah547@gmail.com>
2026-05-19 10:52:18 +0000

    Create AlexLifeExpectancy.py

---
+++ AlexLifeExpectancy.py
@@ -0,0 +1,37 @@
+LifeExpectancy = int(input('Enter Your Age:'))
+if LifeExpectancy == 0:
+    print("You have life expectancy of 63.28 years more")
+elif LifeExpectancy <= 5:
+    print("You have life expectancy of 65.26 years more")
+elif LifeExpectancy <= 9:
+    print("You have life expectancy of 62.14 years more")
+elif LifeExpectancy <= 14:
+    print("You have life expectancy of 57.61 years more")
+elif LifeExpectancy <= 19 :
+    print("You have life expectancy of 48.5 years more")
+elif LifeExpectancy <= 24:
+    print("You have life expectancy of 44.29 years more")
+elif LifeExpectancy <= 29:
+    print("You have life expectancy of 40.02 years more")
+elif LifeExpectancy <= 34:
+    print("You have life expectancy of 35.72 years more")
+elif LifeExpectancy <= 39:
+    print("You have life expectancy of 31.32 years more")
+elif LifeExpectancy <= 44:
+    print("You have life expectancy of 27.13 years more")
+elif LifeExpectancy <= 49:
+    print("You have life expectancy of 23.07 years more")
+elif LifeExpectancy <= 54:
+    print("You have life expectancy of 19.31 years more")
+elif LifeExpectancy <= 59:
+    print("You have life expectancy of 15.71 years more")
+elif LifeExpectancy <= 64:
+    print("You have life expectancy of 12.54 years more")
+elif LifeExpectancy <= 69:
+    print("You have life expectancy of 9.88 years more")
+elif LifeExpectancy <= 74:
+    print("You have life expectancy of 9.00 years more")
+elif LifeExpectancy <=79:
+    print("You have life expectancy of 7.92 years more")
+elif LifeExpectancy >= 80:
+    print("You have life expectancy of 7.4 years more")
